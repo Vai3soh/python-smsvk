4a949df9001b16e97b04d5101031d5d66731aa1b
Vai3soh <work.rwx.seven@gmail.com>
2022-04-07 21:10:35 +1000

    Create  __init__.py
    Imports modules:
    
    Api
    GetAvaibleService
    GetBalance
    GetCountNumberService
    GetNumber
    SetStatusActivation
    GetCodeActivation

---
+++ smsvk/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .project.api_async import Api
+from .project.actions import GetAvaibleService, \
+    GetBalance, \
+    GetCountNumberService, \
+    GetNumber, \
+    SetStatusActivation, GetCodeActivation
